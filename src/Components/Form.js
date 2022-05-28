16c3be9208dae61860db4e42e2e5991450914a5c
Sanjeev <umarsanjeev@gmail.com>
2022-05-28 15:05:34 +0530

    minor fix

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Card from "./Card";
-import styled from "styled-components";
+
 import { MainContainer, Title } from "./Styles";
 
 const Form = () => {
